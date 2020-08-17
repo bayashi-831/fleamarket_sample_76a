4bb9f37561c87d82ea49467740c781e0badd3e7b
Hiromu <hiromu.koumi@gmail.com>
2020-08-17 18:36:27 +0900

    deviseの導入、userモデル、usersテーブルの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,11 @@
 class ApplicationController < ActionController::Base
 
-  before_action :basic_auth, if: :production?
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
+  protected
 
-  def production?
-    Rails.env.production?
+  def configure_permitted_paramaters
+    devise_paramater_sanitizer.permit(:sign_up, keys: [:nickname])
   end
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.credentials[:basic_auth][:user] &&
-      password == Rails.application.credentials[:basic_auth][:pass]
-    end
-  end
-
 end
