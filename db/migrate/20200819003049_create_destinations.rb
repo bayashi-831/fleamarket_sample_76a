26a5125acd820526c9ce19472714bce270827bf5
4429-wd <w4429d0gken@gmail.com>
2020-08-21 18:29:25 +0900

    fix&validates

--- db/migrate/20200819003049_create_destinations.rb
+++ db/migrate/20200819003049_create_destinations.rb
@@ -1,10 +1,11 @@
 class CreateDestinations < ActiveRecord::Migration[5.2]
   def change
     create_table :destinations do |t|
-      t.integer :postal_code, null: false
+      t.string :phone_number 
+      t.string :postal_code, null: false
       t.integer :prefecture_id, null: false
       t.string :city, null: false
-      t.integer :house_number, null: false
+      t.string :house_number, null: false
       t.string :building_name
       t.integer :user_id, null: false, foreign_key: true
       t.string :family_name, null: false
--- db/migrate/20200819090421_devise_create_users.rb
+++ db/migrate/20200819090421_devise_create_users.rb
@@ -4,14 +4,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :nickname, null: false
-      t.string :password, null: false
-      t.string :icon, null: false
+      t.string :icon
       t.string :family_name, null: false
       t.string :first_name, null: false
       t.string :family_name_kana, null: false
       t.string :first_name_kana, null: false
-      t.string :phone_number, null: false
-      t.integer :birthday, null: false
+      # t.string :phone_number 
+      t.date :birthday, null: false
       t.text :introduction
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
