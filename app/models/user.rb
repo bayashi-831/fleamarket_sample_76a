0eb4dd993abd3ca43d349c4a7d6061ee3ce0d315
bayashi-831 <kuroneko.love.takkyubin@gmail.com>
2020-08-21 20:51:36 +0900

    operation-check-after-merging

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
- user-registration-front
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -10,18 +9,6 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
-  VALID_EMAIL_REGEX = /\A[\w+-.]+@[a-z\d-.]+.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
-  validates :nickname, presence: true
-  validates :password, presence: true, length: { minimum: 7}
-  validates :family_name, presence: true,
-  format: { with: /[^ -｡-ﾟ]+/}
-  validates :first_name, presence: true,
-  format: { with: /[^ -｡-ﾟ]+/}
-  validates :family_name_kana, presence: true,
-  format: { with: /[^ -｡-ﾟ]+/}
-  validates :first_name_kana, presence: true,
-  format: { with: /[^ -｡-ﾟ]+/}
-  validates :birthday, presence: true
+
  
 end
