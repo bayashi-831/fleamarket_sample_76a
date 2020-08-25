f48bac4c0435ab88a0ba433f52a4d1887359dfd4
SoraInoue <66369807+SoraInoue@users.noreply.github.com>
2020-08-25 22:11:35 +0900

    確認

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   # devise_for :users
 root 'items#index'
 resources :items, only: [:show,:new] do
-  collection do
-    get 'genre_children', defaults: { format: 'json' }
-    get 'genre_grandchildren', defaults: { format: 'json' }
+    collection do
+      get 'genre_children', defaults: { format: 'json' }
+      get 'genre_grandchildren', defaults: { format: 'json' }
+    end
+    member do
+      get 'genre_children', defaults: { format: 'json' }
+      get 'genre_grandchildren', defaults: { format: 'json' }
+    end
   end
-  member do
-    get 'genre_children', defaults: { format: 'json' }
-    get 'genre_grandchildren', defaults: { format: 'json' }
-  end
-end
 end
