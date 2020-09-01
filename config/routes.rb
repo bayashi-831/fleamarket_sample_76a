217b45aab0ac2b092a3f8cf58ac922cbf45ecc1e
Hiromu <hiromu.koumi@gmail.com>
2020-09-01 11:53:49 +0900

    r

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 devise_for :users
+devise_scope :user do
+  get 'my_page' => 'users/registrations#my_page'
+  get 'logout_page' => 'users/registrations#logout_page'
+end
 root 'items#index'
 devise_scope :user do
   get 'mypage' => 'users/registrations#mypage'
