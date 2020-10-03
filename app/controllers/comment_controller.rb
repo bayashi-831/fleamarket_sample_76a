bcbb272a720cc88ac217f2e253644d6a8fcd1f33
shinnosuke-murakami <iidabasuke@gmail.com>
2020-10-03 12:26:40 +0900

    コメント修正しました。

--- app/controllers/comment_controller.rb
+++ app/controllers/comment_controller.rb
@@ -1,7 +1,15 @@
 class CommentController < ApplicationController
+  before_action :set_item
+
   def create
-    comment = Comment.create(comment_params)
-    redirect_to "/items/#{comments.item.id}" 
+    comment = Comment.new(comment_params)
+    if comment.save
+      redirect_to item_path(@item), notice: 'コメントが保存されました'
+    else
+      comment = @item.comments.includes(:user)
+      flash.now[:alert] = "保存できていません"
+      redirect_to item_path(params[:id])
+    end
   end
 
   private
@@ -9,4 +17,7 @@ class CommentController < ApplicationController
     params.require(:comment).permit(:comment).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
 end
