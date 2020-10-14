d7d0cb5d8801abe14fba82e0d4763971741982de
Hiromu <hiromu.koumi@gmail.com>
2020-10-14 22:23:48 +0900

    修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
 
   before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
+  # before_action :configure_account_update_params, if: :devise_controller?
   def after_sign_in_path_for(resource)
     items_path
   end
@@ -12,6 +13,8 @@ class ApplicationController < ActionController::Base
       keys: [:birthday, :phone_number, :family_name, :first_name, :family_name_kana, :first_name_kana,  
         destination_attributes: [:destination_family_name, :destination_first_name, :destination_family_name_kana, :destination_first_name_kana, :postal_code, :prefecture_id, :city, :street_block, :mansion_name, :nickname]])
   end
+
+  
 
   def production?
     Rails.env.production?
